76dd429f280f042f231b508bd4f89b2f33e5818a
Domas <114532410+BirdyMaster@users.noreply.github.com>
2022-10-18 11:49:43 +0200

    Add final JS

--- rockPaperScissors/java.js
+++ rockPaperScissors/java.js
@@ -5,21 +5,18 @@ window.addEventListener('DOMContentLoaded', () => {
     chooseComputer();
 })
 
-const choiceBtns = document.querySelectorAll('button')
+const choiceBtns = document.querySelectorAll('.chooseBtn')
+const userScoreText = document.querySelector('.userScoreText')
+const computerScoreText = document.querySelector('.computerScoreText')
+const alertText = document.querySelector('.alertText')
+const endScreen = document.querySelector('.endScreen')
+const restartBtn = document.querySelector('.restartBtn')
+
+const userWindow = document.querySelector('.score.user')
+const compWindow = document.querySelector('.score.comp')
+const endText = document.querySelector('.endText')
+
 let choices = ['R', 'P', 'S']
-// let combos = [
-//     ['R', 'P'],
-//     ['R', 'S'],
-//     ['R', 'R'],
-
-//     ['P', 'R'],
-//     ['P', 'S'],
-//     ['P', 'P'],
-
-//     ['S', 'R'],
-//     ['S', 'P'],
-//     ['S', 'S']
-// ]
 let winningCombos = [
     ['R', 'S'],
     ['P', 'R'],
@@ -33,19 +30,22 @@ let drawCombos = [
     ['S', 'S']
 ]
 
-let gameRound;
 let computerChoice;
 let playerChoice;
+
 let computerScore;
 let playerScore;
 
 
 
 function startGame() {
-    gameRound = 0
     computerScore = 0
     playerScore = 0
     chooseComputer()
+    endScreen.classList.remove ('show')
+
+    userScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
 }
 
 function chooseComputer() {
@@ -56,46 +56,74 @@ function chooseComputer() {
 
 choiceBtns.forEach(btn => btn.addEventListener('click', (e) => {
     const clickedBtn = e.target
-    const userChoice = clickedBtn.classList[0]
+    const userChoice = clickedBtn.classList[1]
     compareChoices(userChoice, computerChoice)
 
     //reset compChoice for next click
+    checkForWin()
     chooseComputer()
 }))
 
-
-
-
 function compareChoices(userChoice, computerChoice) {
-    console.log('User ==> ' + userChoice);
-    console.log('Comp ==> ' + computerChoice);
 
     let userWin = winningCombos.some(combo => 
         combo[0] == userChoice && combo[1] == computerChoice)
-
     let draw = drawCombos.some(combo => 
         combo[0] == userChoice && combo[1] == computerChoice)
 
-
+    //either user wins, draws, or loses
     if (userWin) {
-    console.log('User wins')
+        playerScore++;
+        userScoreText.textContent = playerScore;
+
+        userWindow.classList.add('green')
+        setTimeout(() => {
+            userWindow.classList.remove('green')
+        }, 1000);
+
    } else if (draw) {
-     console.log('Draw')
+        alertText.textContent = "It's a draw"
+        alertText.classList.add ('show')
+        setTimeout(() => {
+            alertText.classList.remove ('show')
+        }, 1000);
+
+        userWindow.classList.add('yellow')
+        compWindow.classList.add('yellow')
+        setTimeout(() => {
+            userWindow.classList.remove('yellow')
+            compWindow.classList.remove('yellow')
+        }, 1000);
+
    }  else {
-      console.log('Comp wins');
+        computerScore++;
+        computerScoreText.textContent = computerScore;
+
+        compWindow.classList.add('red')
+        setTimeout(() => {
+            compWindow.classList.remove('red')
+        }, 1000);
    }
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
+
 }
 
+function checkForWin() {
+    if (computerScore == 3 || playerScore == 3) {
+        gameOver()
+    }
+}
 
-// function gameOver(params) {
-    
-// }
+function gameOver() {
+    endScreen.classList.add ('show')
+
+    if (playerScore == 3) {
+        endText.textContent = 'Sweet victory baby!'
+    } else if (computerScore == 3) {
+        endText.textContent = 'The computers are taking over!'
+    }
+}
+
+restartBtn.addEventListener('click', startGame)
+
+
 
